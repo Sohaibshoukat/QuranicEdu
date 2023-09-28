fab08183addaa4f12bc9be976e5a72f2d763645c
Sohaib Shoukat <sohaibshoukat94@gmail.com>
2023-09-28 20:23:29 +0500

    push

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import CommonBanner from "@/Components/CommonBanner"
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <CommonBanner Title="About Us" />
+    </>
   )
 }
 
