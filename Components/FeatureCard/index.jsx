da0eda456167b5b248698ad05c2a0d1f53a721de
Hammad Hassan <99682833+Hammad-Hassan61@users.noreply.github.com>
2023-09-30 01:23:34 +0500

    About Page complete

---
+++ Components/FeatureCard/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.scss";
+
+const FeatureCard = ({buttonText}) => {
+  return (
+<div className="FeatureCardcontainer">
+
+    <div className="row">
+        <img src="./assets/Images/free.png"></img>
+        <p>3-days Free Trial</p>
+    </div>
+
+    <div className="row">
+        <img src="./assets/Images/male.png"></img>
+        <p>Male and Female tutors</p>
+    </div>
+
+    <div className="row">
+        <img src="./assets/Images/duration.png"></img>
+        <p>30-minutes duration</p>
+    </div>
+
+    <div className="row">
+        <img src="./assets/Images/age.png"></img>
+        <p>No age limit</p>
+    </div>
+
+    <div class="white-line"></div>
+
+    <div className="Cardbtn"><button className="Featurebtn">{buttonText}</button></div>
+</div>
+  );
+};
+
+export default FeatureCard;
--- app/About/page.jsx
+++ app/About/page.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import CommonBanner from "@/Components/CommonBanner"
+import AboutUsText from '@/Components/AboutUsText'
+import Feature from '@/Components/Features'
+import TeacherAboutpg from '@/Components/TeacherAboutpg'
+
 
 const page = () => {
   return (
     <>
       <CommonBanner Title="About Us" />
+      <AboutUsText/>
+      <Feature/>
+      <TeacherAboutpg/>
     </>
   )
 }
